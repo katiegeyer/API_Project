98b83c5fdda54f16eac9240ea98aa8e1fec6f2e3
Katie Geyer <keg233@nau.edu>
2023-03-21 12:00:04 -0700

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -56,5 +56,25 @@ router.delete(
     }
 );
 
+// Restore session user
+router.get(
+    '/',
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            const safeUser = {
+                id: user.id,
+                email: user.email,
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+            };
+            return res.json({
+                user: safeUser
+            });
+        } else return res.json({ user: null });
+    }
+);
+
 
 module.exports = router;
