485dad12c73b79874d712ede44240411f04aa624
Katie Geyer <keg233@nau.edu>
2023-03-27 01:18:09 -0700

    one more shot

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -369,6 +369,8 @@ router.post('/:eventId/attendance', requireAuth, handleValidationErrors, async (
             userId: user.id,
             eventId,
             status: 'Pending'
+        }, {
+            fields: ['id', 'eventId', 'userId', 'status', 'createdAt', 'updatedAt']
         })
         return res.status(200).json({
             userId: newAttendance.userId,
