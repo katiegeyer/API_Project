2cae5d7ede490117712e3bec4db757db9f47d4c3
Katie Geyer <keg233@nau.edu>
2023-04-10 08:14:28 -0700

    modals and nav dropdown menu

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,50 +1,41 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useModal } from "../../context/Modal";
 import * as sessionActions from "../../store/session";
-import "./SignupForm.css";
+import './SignupForm.css';
 
-function SignupFormPage() {
+function SignupFormModal() {
     const dispatch = useDispatch();
-    const sessionUser = useSelector((state) => state.session.user);
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-    const [errors, setErrors] = useState({});
-
-    if (sessionUser) return <Redirect to="/" />;
+    const [errors, setErrors] = useState([]);
+    const { closeModal } = useModal();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (password === confirmPassword) {
-            setErrors({});
-            return dispatch(
-                sessionActions.signup({
-                    email,
-                    username,
-                    firstName,
-                    lastName,
-                    password,
-                })
-            ).catch(async (res) => {
-                const data = await res.json();
-                if (data && data.errors) {
-                    setErrors(data.errors);
-                }
-            });
+            setErrors([]);
+            return dispatch(sessionActions.signup({ email, username, firstName, lastName, password }))
+                .then(closeModal)
+                .catch(async (res) => {
+                    const data = await res.json();
+                    if (data && data.errors) setErrors(data.errors);
+                });
         }
-        return setErrors({
-            confirmPassword: "Confirm Password field must be the same as the Password field"
-        });
+        return setErrors(['Confirm Password field must be the same as the Password field']);
     };
 
     return (
         <>
             <h1>Sign Up</h1>
             <form onSubmit={handleSubmit}>
+                <ul>
+                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                </ul>
                 <label>
                     Email
                     <input
@@ -54,7 +45,6 @@ function SignupFormPage() {
                         required
                     />
                 </label>
-                {errors.email && <p>{errors.email}</p>}
                 <label>
                     Username
                     <input
@@ -64,7 +54,6 @@ function SignupFormPage() {
                         required
                     />
                 </label>
-                {errors.username && <p>{errors.username}</p>}
                 <label>
                     First Name
                     <input
@@ -74,7 +63,6 @@ function SignupFormPage() {
                         required
                     />
                 </label>
-                {errors.firstName && <p>{errors.firstName}</p>}
                 <label>
                     Last Name
                     <input
@@ -84,7 +72,6 @@ function SignupFormPage() {
                         required
                     />
                 </label>
-                {errors.lastName && <p>{errors.lastName}</p>}
                 <label>
                     Password
                     <input
@@ -94,7 +81,6 @@ function SignupFormPage() {
                         required
                     />
                 </label>
-                {errors.password && <p>{errors.password}</p>}
                 <label>
                     Confirm Password
                     <input
@@ -104,11 +90,10 @@ function SignupFormPage() {
                         required
                     />
                 </label>
-                {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
                 <button type="submit">Sign Up</button>
             </form>
         </>
     );
 }
 
-export default SignupFormPage;
+export default SignupFormModal;
